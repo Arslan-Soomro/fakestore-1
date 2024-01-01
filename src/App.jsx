e9dd72f95012068ad9571792b630f9c1ddeefed7
Arslan <arslanjs.dev@gmail.com>
2024-01-01 10:20:41 +0500

    Add tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <p className="text-red-300">Hello World</p>
+      <p className="text-red-300 font-primary">Hello World</p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["DM Sans", "sans-serif"],
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
